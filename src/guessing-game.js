56614d2216ddb9f7ac11f13c321e420e5a93168b
kanKda <apiecem9@gmail.com>
2019-10-07 14:45:25 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+    }
 
     setRange(min, max) {
-
+        for (let i = min; i <= max; i++) {
+            this.arr.push(i);
+        }
     }
 
     guess() {
-
+        this.middle = Math.round(this.arr.length / 2);
+        return this.arr[this.middle];
     }
 
     lower() {
-
+        this.arr = this.arr.splice(0, this.middle);
     }
 
     greater() {
-
+        this.arr = this.arr.splice(this.middle, this.arr.length)
     }
 }
 
